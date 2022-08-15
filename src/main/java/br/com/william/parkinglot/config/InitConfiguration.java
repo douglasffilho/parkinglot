dd2f5d8b4eae21c0b2231977721ab13cc6f6c1ac
Douglas Fernandes <douglasf.filho@gmail.com>
2022-08-15 08:10:49 -0300

    refactor: make lots quantity dynamic

--- src/main/java/br/com/william/parkinglot/config/InitConfiguration.java
+++ src/main/java/br/com/william/parkinglot/config/InitConfiguration.java
@@ -1,36 +1,62 @@
 package br.com.william.parkinglot.config;
 
+import br.com.william.parkinglot.entity.Car;
 import br.com.william.parkinglot.entity.Lot;
+import br.com.william.parkinglot.repository.CarRepository;
 import br.com.william.parkinglot.repository.LotRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 @Configuration
 @Profile("init")
 public class InitConfiguration implements InitializingBean {
     private static final Logger log = LoggerFactory.getLogger(InitConfiguration.class);
-    private static final int LOTS_AVAILABLE = 5;
+
+    @Value("${LOTS_AVAILABLE:5}")
+    Integer LOTS_AVAILABLE;
 
     @Autowired
-    LotRepository repository;
+    LotRepository lotsRepository;
+
+    @Autowired
+    CarRepository carRepository;
 
     @Override
     public void afterPropertiesSet() {
         log.info("INICIANDO VALIDAÇÃO DE VAGAS");
-        final long contagemDeVagasNoBanco = this.repository.count();
-        if (contagemDeVagasNoBanco != LOTS_AVAILABLE) {
+        final long contagemDeVagasNoBanco = this.lotsRepository.count();
+        if (contagemDeVagasNoBanco != this.LOTS_AVAILABLE) {
             log.info("CRIANDO VAGAS NO BANCO DE DADOS");
-            this.repository.deleteAll();
-            IntStream
-                    .rangeClosed(1, LOTS_AVAILABLE)
-                    .mapToObj(Lot::new)
-                    .forEach(this.repository::save);
+            this.recreateCars();
+            this.recreateLots();
+        }
+    }
+
+    private void recreateCars() {
+        this.carRepository.deleteAll();
+        this.carRepository.saveAll(List.of(
+                new Car("PDU1234", "Nissan Versa 2021", "Prata"),
+                new Car("KGK1020", "Chevrolet Prisma 2015", "Preto"),
+                new Car("PDU3213", "Nissan Versa 2022", "Prata"),
+                new Car("KGK3020", "Chevrolet Cobalt 2015", "Preto"),
+                new Car("KGI1010", "Jeep Renegade 2016", "Vermelho")
+        ));
+    }
+
+    private void recreateLots() {
+        this.lotsRepository.deleteAll();
+        IntStream
+                .rangeClosed(1, this.LOTS_AVAILABLE)
+                .mapToObj(Lot::new)
+                .forEach(this.lotsRepository::save);
 
 //            IntStream
 //                    .rangeClosed(1, LOTS_AVAILABLE)
@@ -42,6 +68,5 @@ public class InitConfiguration implements InitializingBean {
 //
 //                this.repository.save(newLot);
 //            }
-        }
     }
 }
--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO cars (id,plate,model,color) VALUES ('a91d245c-f863-11ec-b939-0242ac120002','PDU1234','Nissan Versa 2021','Prata');
-INSERT INTO cars (id,plate,model,color) VALUES ('32392fe0-08ec-11ed-861d-0242ac120002','KGK1020','Chevrolet Prisma 2015','Preto');
-INSERT INTO cars (id,plate,model,color) VALUES ('3b421908-08ec-11ed-861d-0242ac120002','PDU3213','Nissan Versa 2022','Prata');
-INSERT INTO cars (id,plate,model,color) VALUES ('41035a50-08ec-11ed-861d-0242ac120002','KGK3020','Chevrolet Cobalt 2015','Preto');
-INSERT INTO cars (id,plate,model,color) VALUES ('45ce635e-08ec-11ed-861d-0242ac120002','KGI1010','Jeep Renegade 2016','Vermelho');
