c289d93854246a3e1a856baa138a083f6e7ae23d
Douglas Fernandes <douglasf.filho@gmail.com>
2022-09-09 09:17:00 -0300

    fix: car conflict message

--- src/main/java/br/com/william/parkinglot/exception/CarConflictException.java
+++ src/main/java/br/com/william/parkinglot/exception/CarConflictException.java
@@ -6,6 +6,6 @@ import static java.lang.String.format;
 
 public class CarConflictException extends WebException {
     public CarConflictException(final String message, final String plate) {
-        super(message, HttpStatus.CONFLICT, format("conflict:%s", plate));
+        super(message, HttpStatus.CONFLICT, format("conflict-car:%s", plate));
     }
 }
